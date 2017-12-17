76d644d2951189a6b1da78001c9799d764646086
Guy Umbright <gumbright@gmail.com>
2017-12-16 21:49:30 -0600

    add framework target

---
+++ Example/Seam3/seam3.h
@@ -0,0 +1,19 @@
+//
+//  seam3.h
+//  seam3
+//
+//  Created by Guy Umbright on 12/16/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for seam3.
+FOUNDATION_EXPORT double seam3VersionNumber;
+
+//! Project version string for seam3.
+FOUNDATION_EXPORT const unsigned char seam3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <seam3/PublicHeader.h>
+
+
